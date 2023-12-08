ce9517d633d7abeef57e8e24d74740f60b2f929b
ricardotran92 <quanghung.850621@gmail.com>
2023-12-08 10:36:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,7 @@ input = open('lr_admit.pkl', 'rb')
 model = pkl.load(input)
 
 st.header('Input admission information')
-gre = 
+gre = st.number_input('Insert GRE Score')
+toefl = st.number_input('Insert TOEFL Score')
+uni_rate = st.number_input('Insert University Rating')
+sop = 
