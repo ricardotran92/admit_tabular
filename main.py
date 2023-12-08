059b0e32d8822363a4821be562d8ae6651c21dd8
ricardotran92 <quanghung.850621@gmail.com>
2023-12-08 10:57:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ research = st.radio('Choose Research', [0, 1], index=None)
 if gre is not None and toefl is not None and uni_rate is not None and sop is not None and lor is not None and cgpa is not None and research is not None:
   if st.button('Predict'):
     feature_vector = np.array([gre, toefl, uni_rate, sop, lor, cgpa, research]).reshape(1,-1)
-    label = str(model.predict((feature_vector)[0])[0])
+    label = str((model.predict(feature_vector)[0])[0])
     st.header('Predict')
     st.text(class_list[label])
-    
+
+
